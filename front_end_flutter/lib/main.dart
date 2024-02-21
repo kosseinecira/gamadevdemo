ea0cad71925ddaa62a4c4d462b7b533b9c83a97d
= <=>
2024-02-21 11:15:17 +0100

    Adding Internationalization feature to the app

--- front_end_flutter/lib/main.dart
+++ front_end_flutter/lib/main.dart
@@ -1,33 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:front_end_flutter/MyApp.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Scaffold(
-            body: Center(
-                child: Column(
-      //moving based on Y axis since it's a Column.
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        // styled TextButton Widget
-        TextButton(
-          style: ButtonStyle(
-              foregroundColor: MaterialStatePropertyAll(Colors.white),
-              backgroundColor: MaterialStatePropertyAll(Colors.blue)),
-          onPressed: null,
-          child: Text("Click Me"),
-        ),
-        // Text Widget for a message...
-        Text("The message should appear here...")
-      ],
-    ))));
-  }
 }
